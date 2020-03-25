d41a2015934c14305d0f80fea585b60f6443dc73
Radek <rado11@onet.pl>
2020-03-25 18:06:40 +0100

    Another change in countdown

--- Countdown.java
+++ Countdown.java
@@ -1,7 +1,7 @@
 public class Countdown {
 
     public static void main(String[] args) {
-        for (int i =115; i>=100; i--){
+        for (int i =78; i>=70; i--){
             System.out.println(i);
         }
     }
