10d7f88bd688939d12f84be272390c2e959631dd
Radek <rado11@onet.pl>
2020-03-25 22:27:59 +0100

    worldSplit

---
+++ GitHomeworks/WorldSplit.java
@@ -0,0 +1,12 @@
+public class WorldSplit {
+
+    public static void main(String[] args) {
+
+        String k = "Java Letter Printer Test";
+
+        for (int i=0; i< k.length(); i++){
+            System.out.println(k.charAt(i));
+        }
+
+    }
+}
---
+++ GitHomeworks/WorldSplit2.java
@@ -0,0 +1,10 @@
+public class WorldSplit2 {
+
+    public static void main(String[] args) {
+        String k= "Java Letter Printer Test";
+        k.toCharArray();
+        for (char c: k.toCharArray()){
+            System.out.println(c);
+        }
+    }
+}
