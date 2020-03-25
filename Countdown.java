fc973bb89fdedd84f523f12ca69db71461c5c333
Radek <rado11@onet.pl>
2020-03-25 17:44:06 +0100

    Changing Countdown

--- Countdown.java
+++ Countdown.java
@@ -1,7 +1,7 @@
 public class Countdown {
 
     public static void main(String[] args) {
-        for (int i =10; i>=0; i--){
+        for (int i =115; i>=100; i--){
             System.out.println(i);
         }
     }
