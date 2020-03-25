5e29903f23f2f26a63373a43da5bc46dfca8915c
Radek <rado11@onet.pl>
2020-03-25 18:27:56 +0100

    And another one

--- Countdown.java
+++ Countdown.java
@@ -1,7 +1,7 @@
 public class Countdown {
 
     public static void main(String[] args) {
-        for (int i =78; i>=70; i--){
+        for (int i =21; i>=17; i--){
             System.out.println(i);
         }
     }
