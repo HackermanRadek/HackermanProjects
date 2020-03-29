b666e1111a3ff108a8ad42bc0cb3cd1259ad538d
Radek <rado11@onet.pl>
2020-03-29 12:49:11 +0200

    Random numbers, but why 0?

--- GitHomeworks/RandomNumber.java
+++ GitHomeworks/RandomNumber.java
@@ -1,25 +1,24 @@
 import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomNumber {
     public static void main(String[] args) {
 
+
+
         int[] randoms = new int[10];
 
+        for ( int i =0; i < randoms.length; i++ ){
 
-        randoms[0] = ThreadLocalRandom.current().nextInt(100);
-        randoms[1] = ThreadLocalRandom.current().nextInt(100);
-        randoms[2] = ThreadLocalRandom.current().nextInt(100);
-        randoms[3] = ThreadLocalRandom.current().nextInt(100);
-        randoms[4] = ThreadLocalRandom.current().nextInt(100);
-        randoms[5] = ThreadLocalRandom.current().nextInt(100);
-        randoms[6] = ThreadLocalRandom.current().nextInt(100);
-        randoms[7] = ThreadLocalRandom.current().nextInt(100);
-        randoms[8] = ThreadLocalRandom.current().nextInt(100);
-        randoms[9] = ThreadLocalRandom.current().nextInt(100);
 
-        System.out.println(Arrays.toString(randoms));
-            }
+            randoms [i]= ThreadLocalRandom.current().nextInt(100);
+
+            System.out.println(Arrays.toString(randoms));
 
         }
+
+
+    }
+}
 
