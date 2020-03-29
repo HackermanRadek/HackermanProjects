345c25f9f0686ba6c302687542d5cb9c5d6f7e59
Radek <rado11@onet.pl>
2020-03-29 15:13:12 +0200

    Only one letter

--- GitHomeworks/WorldSplit.java
+++ GitHomeworks/WorldSplit.java
@@ -2,11 +2,11 @@ public class WorldSplit {
 
     public static void main(String[] args) {
 
-        String k = "Java Letter Printer Test";
 
-        for (int i=0; i< k.length(); i++){
-            System.out.println(k.charAt(i));
-        }
+        for (int i=0; i < args.length; i++)
+            
+            System.out.println(args[i].charAt(i));
+
 
     }
 }
