d1b3c940e3e011f14254987ef8c57295415e4cdd
Radek <rado11@onet.pl>
2020-04-22 18:55:40 +0200

    homework set

---
+++ GitHomeworks/Engine.java
@@ -0,0 +1,38 @@
+public class Engine {
+
+    private String name;
+    private int horsePower;
+    private int capacity;
+
+    Engine (String name, int horsePower, int capacity){
+
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
+
+
