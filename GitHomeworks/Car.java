d291d30b0b6d9af8f558352ec44b52f1006a9dd9
Radek <rado11@onet.pl>
2020-04-28 16:59:13 +0200

    Car 1

--- GitHomeworks/Car.java
+++ GitHomeworks/Car.java
@@ -1,19 +1,59 @@
  class Car {
-    private String name;
+
+     private String name;
     private int fuelTankCapacity;
     private int remainingFuelAmount;
-    private boolean started = true;
-    private boolean stopped = true;
+    private boolean isStarted;
+    private Engine diesel;
 
 
-     public Car(String name, Engine diesel, int fuelTankCapacity){
+     public Car(String name, int fuelTankCapacity, Engine diesel) {
 
-        this.name=name;
-        this.fuelTankCapacity=fuelTankCapacity;
-        this.remainingFuelAmount=fuelTankCapacity;
-        this.started=true;
-        this.stopped=true;
+         this.name=name;
+         this.fuelTankCapacity=fuelTankCapacity;
+         this.remainingFuelAmount=fuelTankCapacity;
+         this.isStarted = false;
+     }
 
+     public void startEngine() {
+
+         System.out.println("VW engine - engine start");
+
+         if (isStarted = true){
+             System.out.println("VW engine is already started");
+         }
+
+
+     }
+
+     public void stopEngine(){
+
+
+         System.out.println("VW engine - engine stop");
+
+        if (isStarted=false){
+            System.out.println("VW engine is already stopped");
+        }
+
+     }
+
+
+     public void tank(){
+
+         this.remainingFuelAmount=fuelTankCapacity;
+
+         System.out.println("VW Golf has been tanked up");
+
+     }
+
+    public void drive (int speed, double distance){
+
+         if (speed>diesel.getMaxSpeed()){
+             System.out.println("You can drive only with maximum speed");
+         } else {
+             System.out.println("VW Golf has been driven with a speed of " + speed + " km/h for a distance of "
+             + distance + " km");
+         }
 
 
 
@@ -21,49 +61,8 @@
 
 
 
-    public void tank(int fuelTankCapacity){ ;
+
+ }
 
 
 
-        this.remainingFuelAmount = fuelTankCapacity;
-
-    }  
-
-    public void drive(int speed, int kilometers) {
-
-        stopped=false;
-
-        System.out.println("VW Golf has been driven for " + kilometers + " km with speed " + speed + "km/h");
-
-    }
-
-    public void startEngine(){
-
-        if (started) {
-            System.out.println("VW engine - engine start");
-        }else {
-            System.out.println("VW engine is already started");
-        }
-
-        started=false;
-
-        }
-
-
-
-   public void stopEngine(){
-
-       if (stopped) {
-           System.out.println("VW engine - engine Stop");
-       }else {
-           System.out.println("VW engine is already stopped");
-       }
-
-       stopped=false;
-
-   };
-
-
-
-
-}
