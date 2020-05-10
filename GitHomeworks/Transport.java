ecb9b17497d87916e7cf9bcffc0753c4e82037c1
Radek <rado11@onet.pl>
2020-05-10 15:42:22 +0200

    transporcik

--- GitHomeworks/Transport.java
+++ GitHomeworks/Transport.java
@@ -1,10 +1,20 @@
-public class Transport  {
+public class Transport {
 
-    public void go(){
+
+    public void go() {
         System.out.println("Lets go");
 
+    }   public static boolean isItPublicTransport() {
 
+        if (false){
+            System.out.println("Its not public transport");
+        }else
+            System.out.println("Its public transport");
 
+        return true;
     }
+
+
+
 
 }
