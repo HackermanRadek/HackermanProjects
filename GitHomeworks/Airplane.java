232a76ca62dddd593ddfb3ab9d6a81f8bd3433d2
Radek <rado11@onet.pl>
2020-05-10 13:14:55 +0200

    transporcik

---
+++ GitHomeworks/Airplane.java
@@ -0,0 +1,7 @@
+public class Airplane extends publicTransport {
+    @Override
+    public void go() {
+        super.go();
+        System.out.println("Flying away");
+    }
+}
---
+++ GitHomeworks/Motorbike.java
@@ -0,0 +1,7 @@
+public class Motorbike extends Transport {
+    @Override
+    public void go() {
+        super.go();
+        System.out.println("Easiest way to get killed");
+    }
+}
