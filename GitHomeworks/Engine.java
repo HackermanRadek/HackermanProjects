dfdadc0a18ab15a35706bb84c789f865271d7146
Radek <rado11@onet.pl>
2020-05-12 14:15:10 +0200

    engine

--- GitHomeworks/Engine.java
+++ GitHomeworks/Engine.java
@@ -1,37 +1,104 @@
 public class Engine {
 
+public boolean isStarted;
     private String name;
     private int horsePower;
     private int capacity;
 
-    Engine (String name, int horsePower, int capacity){
+    private int maxSpeed;
 
+    public Engine(String name, int horsePower, int capacity) {
+
+        this.name=name;
+        this.horsePower=horsePower;
+        this.capacity=capacity;
+        this.maxSpeed=2*this.horsePower;
     }
 
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
 
-    public void setHorsePower(int horsePower) {
-        this.horsePower = horsePower;
+
+
+
+    public void startEngine(){
+
+
+
+        if (isStarted){
+            System.out.println("Engine is already started");
+        }else {
+            System.out.println("Engine start");
+        }
+
+        isStarted=true;
+
+    }
+    public void stopEngine(){
+        if (isStarted==false){
+            System.out.println("Engine is already stopped");
+        }else {
+            System.out.println("Engine stop");
+        }
+        isStarted=false;
+
+
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public int getCapacity() {
-        return capacity;
+    public String getName() {
+        return name;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
     }
 
     public int getHorsePower() {
         return horsePower;
     }
 
-    public String getName() {
-        return name;
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
     }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void isStarted(boolean started) {
+        isStarted = started;
+    }
+
+    public boolean isStarted() {
+        return isStarted;
+    }
+
+    public void setStarted(boolean started) {
+        isStarted = started;
+    }
+
+
+
+
+
+
 }
+
+
+
+
+
+
 
 
 
