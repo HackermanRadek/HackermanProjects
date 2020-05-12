ab260bc1f153946c5a7c4999a3803e1fcf079fc4
Radek <rado11@onet.pl>
2020-05-12 17:41:07 +0200

    transport

--- GitHomeworks/Transport.java
+++ GitHomeworks/Transport.java
@@ -1,20 +1,46 @@
-public class Transport {
+public class Transport  {
 
 
     public void go() {
         System.out.println("Lets go");
 
-    }   public static boolean isItPublicTransport() {
-
-        if (false){
-            System.out.println("Its not public transport");
-        }else
-            System.out.println("Its public transport");
-
-        return true;
     }
+    public static boolean isItPublicTransport(){
+
+        if (true){
+            System.out.println("Its not for public");
+        }else {
+            System.out.println("Its for public");
+        }
+
+        return false;
+        }
+    public static boolean flyable(){
+
+        if (true){
+            System.out.println("It can fly");
+        } else {
+            System.out.println("It cannot fly");
+        }
+
+        return false;
+    }
+
+
+    
 
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ GitHomeworks/transportTester.java
@@ -0,0 +1,13 @@
+public class transportTester {
+    public static void main(String[] args)  {
+        Airplane airplane = new Airplane();
+        airplane.go();
+
+
+        Motorbike.flyable();
+        Bus.isItPublicTransport();
+
+    }
+
+    }
+
