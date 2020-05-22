8e9ad26a47eceb66101a17145cc74389f1b13e39
Radek <rado11@onet.pl>
2020-05-22 16:44:47 +0200

    transporcik

--- GitHomeworks/transportTester.java
+++ GitHomeworks/transportTester.java
@@ -1,13 +1,41 @@
+import java.util.ArrayList;
+
 public class transportTester {
+
+
+
     public static void main(String[] args)  {
+
+
         Airplane airplane = new Airplane();
-        airplane.go();
+
+        Motorbike motorbike = new Motorbike();
+
+        Bus bus = new Bus();
+
+        Bicycle bicycle = new Bicycle();
+
+        Train train = new Train();
+
+        Tram tram = new Tram();
 
 
-        Motorbike.flyable();
-        Bus.isItPublicTransport();
 
     }
+
+    public static boolean isItPublic(Transport transport){
+
+        if (transport instanceof publicTransport){
+            System.out.println("Thats public transport");
+        } else {
+            System.out.println("Thats not public transport");
+        }
+
+        return false;
+
+    }
+
+
 
     }
 
