03841133d1ae817b4704cce852530028dff868c0
Radek <rado11@onet.pl>
2020-05-27 18:38:31 +0200

    lists

--- GitHomeworks/Lists.java
+++ GitHomeworks/Lists.java
@@ -6,7 +6,7 @@ public class Lists {
 
     public static void main(String[] args) {
 
-        System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
 
         List<Integer> numbers = new ArrayList<>();
 
@@ -19,27 +19,23 @@ public class Lists {
         System.out.println(numbers);
 
 
+        for (int i = 0; i < 100_000; i++) {
 
-        for (int i = 0; i <100_000 ; i++) {
 
-
-            numbers.add(0,17);
-
+            numbers.add(0, 17);
 
 
         }
 
-        System.currentTimeMillis();
+        long endTime = System.currentTimeMillis();
 
-        System.out.println(System.currentTimeMillis());
+        System.out.println(endTime - startTime);
 
 
         System.out.println("==========================================================================");
 
 
-
-
-        System.currentTimeMillis();
+        long startTimeV2 = System.currentTimeMillis();
 
         List<Integer> numbers2 = new LinkedList<>();
 
@@ -51,15 +47,45 @@ public class Lists {
         System.out.println(numbers2);
 
 
-        for (int i = 0; i <100_000 ; i++) {
+        for (int i = 0; i < 100_000; i++) {
 
-            numbers2.add(0,69);
+            numbers2.add(0, 69);
 
         }
 
-        System.currentTimeMillis();
-        System.out.println(System.currentTimeMillis());
+        long endTimeV2 = System.currentTimeMillis();
+        System.out.println(endTimeV2 - startTimeV2);
+
+
+        System.out.println("===================================================================");
+
+
+        long startV3 = System.currentTimeMillis();
+
+
+        for (int i = 0; i < 100_000; i++) {
+
+
+            numbers.get(i);
+
+        }
+        long endV3 = System.currentTimeMillis();
+
+        System.out.println(endV3 - startV3);
+
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+
+        long startV4 = System.currentTimeMillis();
+
+        for (int i = 0; i < 100_000; i++) {
+
+            numbers2.get(i);
+
+        }
+        long endV4 = System.currentTimeMillis();
+
+        System.out.println(endV4 - startV4);
 
     }
-
 }
