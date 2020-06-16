e9725e67a284cb69d82b97c2970e32419e08ca0b
Radek <rado11@onet.pl>
2020-06-16 15:48:45 +0200

    week

--- GitHomeworks/DaysOfweek/DayOfWeek.java
+++ GitHomeworks/DaysOfweek/DayOfWeek.java
@@ -1,9 +1,10 @@
 import java.util.LinkedList;
 
-public enum DayOfWeek {MON("Poniedziałek", "Monday", true),
+public enum DayOfWeek {
+    MON("Poniedziałek", "Monday", true),
     TUE("Wtorek", "Tuesday", true),
     WED("Środa", "Wednesday", true),
-    THU("Czwartek" , "Thursday", true),
+    THU("Czwartek", "Thursday", true),
     FRI("Piątek", "Friday", true),
     SAT("Sobota", "Saturday", false),
     SUN("Niedziela", "Sunday", false);
@@ -13,100 +14,41 @@ public enum DayOfWeek {MON("Poniedziałek", "Monday", true),
     private boolean workingDay;
 
 
-
-    DayOfWeek(String polishName, String englishName, boolean workingDay){
+    DayOfWeek(String polishName, String englishName, boolean workingDay) {
         this.polishName = polishName;
         this.englishName = englishName;
         this.workingDay = workingDay;
     }
 
-    public boolean isWorkingDay() {
+    public static DayOfWeek fromPolishName(String polish) {
+        for (DayOfWeek dayOfWeek : values()) {
+            if (dayOfWeek.polishName.equalsIgnoreCase(polish)) {
+                return dayOfWeek;
+            }
+        }
+
+        throw new IllegalArgumentException("There is no day with \"" + polish + "\" polish name");
+
+    }
+
+    public static DayOfWeek fromEnglishName(String english) {
+        for (DayOfWeek dayOfWeek : values()) {
+            if (dayOfWeek.englishName.equalsIgnoreCase(english)) {
+                return dayOfWeek;
+            }
+        }
+
+        throw new IllegalArgumentException("There is no day with \"" + english + "\" english name");
+
+    }
+
+
+    public boolean workingDay() {
+
         return workingDay;
     }
 
-    public void setWorkingDay(boolean workingDay) {
-        this.workingDay = workingDay;
-    }
 
-    public String getEnglishName() {
-        return englishName;
-    }
-
-    public void setEnglishName(String englishName) {
-        this.englishName = englishName;
-    }
-
-    public String getPolishName() {
-        return polishName;
-    }
-
-    public void setPolishName(String polishName) {
-        this.polishName = polishName;
-    }
-
-   public static void fromPolishName(DayOfWeek dayOfWeek){
-
-       System.out.println(dayOfWeek.polishName);
-
-   }
-
-   public static void fromEnglishName(DayOfWeek dayOfWeek){
-
-
-       System.out.println(dayOfWeek.englishName);
-
-   }
-
- public static boolean isItWeekend(DayOfWeek dayOfWeek){
-
-        if (dayOfWeek.workingDay){
-            System.out.println("Its not weekend");
-        }else {
-            System.out.println("Its weekend!");
-        }
-
-        return false;
-    }
-
-        private static LinkedList<String> workingDays = new LinkedList<String>();
-    public static void getWorkingDays(){
-
-
-
-        System.out.println("Working days: " + workingDays);
-
-    }
-
-        private static LinkedList<String> weekendDays = new LinkedList<String>();
-
-    public static void getWeekends(){
-
-        System.out.println("Weekend: " + weekendDays);
-    }
-
-
-
-    public static void main(String[] args) {
-
-        weekendDays.add(SAT.englishName);
-        weekendDays.add(SUN.englishName);
-
-
-        workingDays.add(MON.englishName);
-        workingDays.add(TUE.englishName);
-        workingDays.add(WED.englishName);
-        workingDays.add(THU.englishName);
-        workingDays.add(FRI.englishName);
-
-    fromPolishName(MON);
-    fromEnglishName(SAT);
-    isItWeekend(THU);
-    isItWeekend(SUN);
-
-    getWorkingDays();
-    getWeekends();
-
-    }
 
 
 
--- GitHomeworks/DaysOfweek/EnumDemko.java
+++ GitHomeworks/DaysOfweek/EnumDemko.java
@@ -1,15 +1,28 @@
+import java.util.LinkedList;
+
 public class EnumDemko {
 
-        public static final String MONDAY = "MON";
-         public static final String TUESDAY = "TUE";
-        public static final String WEDNESDAY= "WED";
-         public static final String THURSDAY = "THU";
-         public static final String FRIDAY = "FRI";
-         public static final String SATURDAY = "SAT";
-         public static final String SUNDAY = "SUN";
+    public static void main(String[] args) {
+        System.out.println(DayOfWeek.fromPolishName("poniedziałek"));
+        System.out.println(DayOfWeek.fromPolishName("sobota"));
+        System.out.println(DayOfWeek.fromEnglishName("friday"));
+
+
+        DayOfWeek saturday = DayOfWeek.SAT;
+        DayOfWeek monday = DayOfWeek.MON;
+        DayOfWeek tue = DayOfWeek.TUE;
+        DayOfWeek wed = DayOfWeek.WED;
+        DayOfWeek thu = DayOfWeek.THU;
+        DayOfWeek fri = DayOfWeek.FRI;
+        DayOfWeek sun = DayOfWeek.SUN;
+
+
+        System.out.println(monday.workingDay());
+        System.out.println(saturday.workingDay());
 
 
 
 
 
+    }
 }
